f2c6ea5114ff7d0f01181b0e4dcccb4e12f89d08
kIvy1189 <kevindivy@gmail.com>
2020-05-31 17:26:47 -0500

    Added start for delete function

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -8,23 +8,56 @@ router.get("/notes", (req, res) => {
     return res.json(results);
 });
 
+router.get("/notes/:id", (req, res) => {
+    const result = req.params.id;
+
+    for (let i = 0; i < notes.length; i++) {
+        if (result === notes[i].id) {
+          return res.json(notes[i]);
+        }
+      }
+    
+      return res.json(false);
+})
+
 router.post("/notes", (req,res) => {
+    //Create a note using request body
     const newNote = req.body;
 
+    //Add an id based on number of saved notes
     req.body.id = notes.length.toString();
 
+    //Add new note to the array
     notes.push(newNote);
 
+    //Update db file
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
         JSON.stringify(notes, null, 2)
     );
 
+    //reload page to display saved notes
     res.redirect(req.originalUrl)
 });
 
-router.delete("/api/notes/:id", (req, res) => {
-    
+router.delete('/notes/:id', (req, res) => {
+        // read noteData from json file
+        noteData = fs.readFileSync(
+            path.join(__dirname, '../../db/db.json'));
+        // parse data to get array of objects
+        noteData = JSON.parse(noteData);
+        // delete old note from array of note objects
+        noteData = noteData.filter(function(note) {
+            return note.id != req.params.id;
+        });
+
+        //write new notes to the file
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify(noteData, null, 2));
+
+        //reload page to display saved notes
+        res.sendStatus(204)
 });
 
 module.exports = router;
